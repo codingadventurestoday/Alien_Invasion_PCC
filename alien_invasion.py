aa760fa03f5df1897fa9c08d0b57b2e56270f187
Jeremy Phillips <codingadventurestoday@gmail.com>
2024-10-10 10:48:35 -0400

    added movement to the ship through the update method in ship.py and adding key stoke event for right and left inside the check_events function of game_functions.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,7 +2,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
-import game_function as gf
+import game_functions as gf
 
 
 def run_game(): 
@@ -23,8 +23,8 @@ def run_game():
     #start the main loop for the game 
     while True: 
         #watch for keyboard and mouse events
-        gf.check_events()
-
+        gf.check_events(ship)
+        ship.update()
         #redraw the screen each pass through the loop 
         gf.update_screen(ai_settings, screen, ship)
 
--- ship.py
+++ ship.py
@@ -8,11 +8,22 @@ class Ship():
         #load the ship image and get its rect
         self.image = pygame.image.load("./img/ship.bmp")
         self.rect = self.image.get_rect()
-        self.screen_rect - screen.get_rect()
+        self.screen_rect = screen.get_rect()
+
+        #Movement flag
+        self.moving_right = False
+        self.moving_left = False
 
         #start each new ship at the bottom center of the screen
-        self.rect.centerx = self.screen.rect.centerx
+        self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
+
+    def update(self):
+        """Update the ship's position based on movement flag"""
+        if self.moving_right:
+            self.rect.centerx += 1
+        if self.moving_left:
+            self.rect.centerx -= 1
 
     def blitme(self):
         """Draw the ship at its current location"""
